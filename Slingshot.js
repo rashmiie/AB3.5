1aa2390551509c690df70498d0f68074467d38dd
rashmi <rashmi.singh7713@gmail.com>
2020-08-23 22:25:50 +0530

    commit

--- Slingshot.js
+++ Slingshot.js
@@ -11,6 +11,10 @@ class Slingshot{
         World.add(world, this.sling);
     }
 
+    fly(){
+        this.sling.bodyA= null;
+    }
+
     display(){
         if(this.sling.bodyA){
         var pointA = this.sling.bodyA.position;
